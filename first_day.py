1f4b495fa6231ed217d5c11eeda5bbd2356ef35e
Alex <oleksandr.hlukhoman@dci-student.org>
2023-10-19 13:33:02 +0200

    al

---
+++ first_day.py
@@ -0,0 +1,30 @@
+# print("Hallo World!")
+# print ("the itsy bitsy spider climbet up the waterspun")
+# name_2005 = "Veronika"
+# first_name = "Hlukhoman"
+# age = 43
+# print(type(age))
+# count_mw = 45000.50
+# print(type(count_mw))
+
+'''
+thi is first line comment
+this is second line comment
+this is third comment
+
+'''
+# print("what is your name: ")
+
+# name = input()
+
+# print(name)
+# print("what is your name: ")
+
+# school_name = input()
+# print (school_name.isupper())
+
+# def isupper():
+   # print("some word:")
+   # input()
+some_name = input()
+print("This is the value your contered:" , some_name)   
